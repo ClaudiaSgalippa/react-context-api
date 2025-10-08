57bedd1cc4f23780cba1c2a89aad65c24d99de59
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-08 22:41:58 +0200

    Modifica commento costum hook

--- src/contetx/BudgetContext.jsx
+++ src/contetx/BudgetContext.jsx
@@ -2,7 +2,7 @@ import {createContext, useContext, useState} from "react"; {/*Import degli hook 
 
 const BudgetContext = createContext(); {/*Creazione del contesto*/}
 
-function BudgetProvider ({children}) { {/*Definiamo il PROVIDER e quali "feature" vogliamo trasmettere (rendere disponibili)*/}
+function BudgetProvider ({children}) { {/*Definiamo un costum hook da esportare per "consumare" il contesto*/}
     const [budgetMode, setBudgetMode] = useState(false); {/*I dati che vogliamo trasmettere (le variabili di stato, il VALUE), in questo caso il valore booleano*/}
 
     return (
